cddd2d2c432fd0df4dfb2b4fbb77e84c4df0c349
mihinov <kamahinmihail@gmail.com>
2025-06-27 16:31:04 +0300

    feat: настройка запуска из docker-compose

--- utils/create-env.js
+++ utils/create-env.js
@@ -1,26 +1,39 @@
+// utils/create-env.js
 const fs = require('fs');
 const path = require('path');
 
 const envContent = `
+# MongoDB
 MONGODB_USER_NAME=admin
 MONGODB_USER_PASSWORD=пароль
 MONGODB_HOST=127.0.0.1
 MONGODB_PORT=29017
 MONGODB_DATABASE_NAME=intellectika
-PROJECT_NAME=intellectika
 MONGODB_DATABASE_AUTH_NAME=admin
+
+# Backend
+PROJECT_NAME=intellectika
 PORT=4336
 JWT_SECRET=secret-hello
+
+# Frontend
+FRONTEND_PORT=4700
+FRONTEND_ADMIN_PORT=4600
 `.trim();
 
-const backendDir = path.join(__dirname, '..', 'backend');
-const envPath = path.join(backendDir, '.env');
+// Пути к файлам .env
+const paths = [
+  path.join(__dirname, '..', '.env'),
+  path.join(__dirname, '..', 'backend', '.env')
+];
 
-// Убедимся, что папка backend существует
-if (!fs.existsSync(backendDir)) {
-  fs.mkdirSync(backendDir, { recursive: true });
-}
+// Создаём файлы
+paths.forEach(envPath => {
+  const dir = path.dirname(envPath);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
 
-fs.writeFileSync(envPath, envContent, 'utf8');
-
-console.log(`.env файл успешно создан по пути: ${envPath}`);
+  fs.writeFileSync(envPath, envContent, 'utf8');
+  console.log(`.env файл успешно создан по пути: ${envPath}`);
+});
